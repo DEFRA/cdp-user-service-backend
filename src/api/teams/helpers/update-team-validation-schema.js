2af1bc27c836ae4b7e5455ad2388e473ac8ae804
Ben Chidgey <ben@feedmypixel.com>
2023-08-23 09:53:02 +0100

    Remove optional
    Joi strings are optional by default

--- src/api/teams/helpers/update-team-validation-schema.js
+++ src/api/teams/helpers/update-team-validation-schema.js
@@ -3,9 +3,8 @@ import Joi from 'joi'
 const updateTeamValidationSchema = Joi.object({
   name: Joi.string()
     .max(53)
-    .regex(/^[A-Za-z0-9-]+$/)
-    .optional(),
-  description: Joi.string().optional()
+    .regex(/^[A-Za-z0-9-]+$/),
+  description: Joi.string().allow(null)
 })
 
 export { updateTeamValidationSchema }
