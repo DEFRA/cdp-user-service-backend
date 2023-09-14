fa56ea5164acab1e48388cd28590f70aadfd048f
Anargyros Akrivos <argy@akrivos.me>
2023-09-14 15:31:32 +0100

    Redact cookies and passwords

--- src/helpers/request-logger.js
+++ src/helpers/request-logger.js
@@ -7,6 +7,10 @@ const requestLogger = {
   plugin: hapiPino,
   options: {
     enabled: !appConfig.get('isTest'),
+    redact: {
+      paths: ['req.headers.authorization', 'req.headers.cookie', 'res.headers'],
+      remove: true
+    },
     level: appConfig.get('logLevel'),
     ...(appConfig.get('isDevelopment')
       ? { transport: { target: 'pino-pretty' } }
