62982d1438448afe35602e9b61481d0e6ba5d76e
Anargyros Akrivos <argy@akrivos.me>
2023-10-06 09:54:59 +0100

    Disabled auth temporarily

--- src/api/teams/controllers/add-user-to-team.js
+++ src/api/teams/controllers/add-user-to-team.js
@@ -1,21 +1,21 @@
 import Boom from '@hapi/boom'
 import { isNull } from 'lodash'
 
-import { appConfig } from '~/src/config'
+// import { appConfig } from '~/src/config'
 import { getTeam } from '~/src/api/teams/helpers/get-team'
 import { getUser } from '~/src/api/users/helpers/get-user'
 import { teamHasUser } from '~/src/api/teams/helpers/team-has-user'
 import { addUserToTeam } from '~/src/api/teams/helpers/add-user-to-team'
 
 const addUserToTeamController = {
-  options: {
-    auth: {
-      strategy: 'azure-oidc',
-      access: {
-        scope: [appConfig.get('azureAdminGroupId'), '{params.teamId}']
-      }
-    }
-  },
+  // options: {
+  //   auth: {
+  //     strategy: 'azure-oidc',
+  //     access: {
+  //       scope: [appConfig.get('azureAdminGroupId'), '{params.teamId}']
+  //     }
+  //   }
+  // },
   handler: async (request, h) => {
     const teamId = request.params.teamId
     const userId = request.params.userId
