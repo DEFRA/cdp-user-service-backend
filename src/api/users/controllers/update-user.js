d29275d1db435feb6277b0005574ba396953a749
Ivar Abrahamsen <ivar.abrahamsen@defra.gov.uk>
2024-06-04 13:22:50 +0100

    Mongo lock on update users (#93)
    * Mongo lock on update users
    
    * Wrong method. And email not strict locally
    
    * Only log if logger is thruthy
    
    * Try catch locking

--- src/api/users/controllers/update-user.js
+++ src/api/users/controllers/update-user.js
@@ -7,11 +7,12 @@ import { getUser } from '~/src/api/users/helpers/get-user'
 import { buildUpdateFields } from '~/src/helpers/build-update-fields'
 import { gitHubUserExists } from '~/src/api/users/helpers/github-user-exists'
 import { updateUser } from '~/src/api/users/helpers/update-user'
+import { requireLock } from '~/src/helpers/mongo-lock'
 
 const updateUserController = {
   options: {
     validate: {
-      payload: updateUserValidationSchema
+      payload: updateUserValidationSchema(config.get('isProduction'))
     },
     auth: {
       strategy: 'azure-oidc',
@@ -46,7 +47,13 @@ const updateUserController = {
       }
     }
 
-    const updatedUser = await updateUser(request.db, userId, updateFields)
+    const lock = await requireLock(request.locker, 'users')
+    let updatedUser
+    try {
+      updatedUser = await updateUser(request.db, userId, updateFields)
+    } finally {
+      lock.free()
+    }
     return h.response({ message: 'success', user: updatedUser }).code(200)
   }
 }
