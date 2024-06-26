37a11fb4132d1fa7fc3138a7e58f36fcd63170b8
Ben Chidgey <ben@feedmypixel.com>
2024-06-26 17:31:34 +0100

    Setup wreck proxy agents

--- src/helpers/proxy/proxy-agent.js
+++ src/helpers/proxy/proxy-agent.js
@@ -5,10 +5,12 @@ import { config } from '~/src/config'
 
 const proxyAgent = () => {
   const proxy = config.get('httpsProxy') ?? config.get('httpProxy')
+
   if (!proxy) {
     return null
   } else {
     const proxyUrl = new URL(proxy)
+
     return {
       url: proxyUrl,
       agent: new HttpsProxyAgent(proxyUrl)
