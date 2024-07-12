a1407efd91b1b8829f6920a6ab8d754af3e1ab30
Ben Chidgey <ben@feedmypixel.com>
2024-07-12 08:00:32 +0100

    Set msgraph base url default to dev url

--- src/config/index.js
+++ src/config/index.js
@@ -99,7 +99,7 @@ const config = convict({
     doc: 'MsGraph api endpoint',
     format: String,
     env: 'AZURE_CLIENT_BASE_URL',
-    default: ''
+    default: 'http://localhost:3939/msgraph/'
   },
   oidcWellKnownConfigurationUrl: {
     doc: 'OIDC .well-known configuration URL',
