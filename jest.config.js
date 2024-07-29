6543c84d75c31b03c8fc24f9c6a222a3e883e58f
Ben Chidgey <ben@feedmypixel.com>
2024-07-29 10:40:17 +0100

    Set tests to log when in development

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   verbose: true,
   resetModules: true,
   clearMocks: true,
-  silent: true,
+  silent: process.env.NODE_ENV !== 'development',
   preset: '@shelf/jest-mongodb',
   testMatch: ['**/src/**/*.test.js'],
   reporters: ['default', ['github-actions', { silent: false }], 'summary'],
