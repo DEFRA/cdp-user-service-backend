88849917738877ef4042be5644a8cbda858b2f34
Ben Chidgey <ben@feedmypixel.com>
2024-08-16 17:36:50 +0100

    Turn on logging in tests

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   verbose: true,
   resetModules: true,
   clearMocks: true,
-  silent: process.env.NODE_ENV !== 'development',
+  silent: false,
   preset: '@shelf/jest-mongodb',
   testMatch: ['**/src/**/*.test.js'],
   reporters: ['default', ['github-actions', { silent: false }], 'summary'],
