1ccefa5ed1a204e545efbb0aa82a781e79ca67e2
Ben Chidgey <ben@feedmypixel.com>
2024-11-20 11:37:35 +0000

    Remove previous tracing work

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -5,51 +5,48 @@ import { getUsersController } from '~/src/api/users/controllers/get-users.js'
 import { updateUserController } from '~/src/api/users/controllers/update-user.js'
 import { getAadUsersController } from '~/src/api/users/controllers/get-aad-users.js'
 import { getGitHubUsersController } from '~/src/api/users/controllers/get-github-users.js'
-import { withTracing } from '~/src/helpers/tracing/tracing.js'
 
 const users = {
   plugin: {
     name: 'users',
     register: (server) => {
-      server.route(
-        [
-          {
-            method: 'GET',
-            path: '/users',
-            ...getUsersController
-          },
-          {
-            method: 'POST',
-            path: '/users',
-            ...createUserController
-          },
-          {
-            method: 'GET',
-            path: '/users/{userId}',
-            ...getUserController
-          },
-          {
-            method: 'PATCH',
-            path: '/users/{userId}',
-            ...updateUserController
-          },
-          {
-            method: 'DELETE',
-            path: '/users/{userId}',
-            ...deleteUserController
-          },
-          {
-            method: 'GET',
-            path: '/aad-users',
-            ...getAadUsersController
-          },
-          {
-            method: 'GET',
-            path: '/github-users',
-            ...getGitHubUsersController
-          }
-        ].map(withTracing)
-      )
+      server.route([
+        {
+          method: 'GET',
+          path: '/users',
+          ...getUsersController
+        },
+        {
+          method: 'POST',
+          path: '/users',
+          ...createUserController
+        },
+        {
+          method: 'GET',
+          path: '/users/{userId}',
+          ...getUserController
+        },
+        {
+          method: 'PATCH',
+          path: '/users/{userId}',
+          ...updateUserController
+        },
+        {
+          method: 'DELETE',
+          path: '/users/{userId}',
+          ...deleteUserController
+        },
+        {
+          method: 'GET',
+          path: '/aad-users',
+          ...getAadUsersController
+        },
+        {
+          method: 'GET',
+          path: '/github-users',
+          ...getGitHubUsersController
+        }
+      ])
     }
   }
 }
