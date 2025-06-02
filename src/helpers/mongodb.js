8c92fe5e6bbe9ac15ff26b4c61c6726339aa4149
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2025-06-02 09:41:23 +0100

    Reworks mongo config/options

--- src/helpers/mongodb.js
+++ src/helpers/mongodb.js
@@ -1,49 +1,36 @@
 import { MongoClient } from 'mongodb'
 import { LockManager } from 'mongo-locks'
 
-function mongoPlugin(config) {
-  return {
-    plugin: {
-      name: 'mongodb',
-      version: '1.0.0',
-      register: async function (server, options) {
-        server.logger.info('Setting up mongodb')
+const mongoPlugin = {
+  name: 'mongodb',
+  version: '1.0.0',
+  register: async function (server, options) {
+    server.logger.info('Setting up mongodb')
+    const uri = options.mongoUrl
+    const client = await MongoClient.connect(uri, {
+      ...options.mongoOptions,
+      ...(server.secureContext && { secureContext: server.secureContext })
+    })
+    const databaseName = options.databaseName
+    const db = client.db(databaseName)
+    const locker = new LockManager(db.collection('mongo-locks'))
 
-        const uri = config.get('mongoUri')
-        const client = await MongoClient.connect(uri, {
-          ...options.mongoOptions,
-          ...(server.secureContext && { secureContext: server.secureContext })
-        })
-        const databaseName = config.get('mongoDatabase')
-        const db = client.db(databaseName)
-        const locker = new LockManager(db.collection('mongo-locks'))
+    await createIndexes(db)
 
-        await createIndexes(db)
+    server.logger.info(`mongodb connected to ${databaseName}`)
 
-        server.logger.info(`mongodb connected to ${databaseName}`)
+    server.decorate('server', 'mongoClient', client)
+    server.decorate('request', 'mongoClient', client)
 
-        server.decorate('server', 'mongoClient', client)
-        server.decorate('request', 'mongoClient', client)
+    server.decorate('server', 'db', db)
+    server.decorate('request', 'db', db)
 
-        server.decorate('server', 'db', db)
-        server.decorate('request', 'db', db)
+    server.decorate('request', 'locker', locker)
 
-        server.decorate('request', 'locker', locker)
-
-        server.events.on('stop', async () => {
-          server.logger.info(`Closing Mongo client`)
-          await client.close(true)
-        })
-      }
-    },
-    options: {
-      mongoUrl: config.get('mongoUri'),
-      databaseName: config.get('mongoDatabase'),
-      mongoOptions: {
-        retryWrites: false,
-        readPreference: 'secondary'
-      }
-    }
+    server.events.on('stop', async () => {
+      server.logger.info(`Closing Mongo client`)
+      await client.close(true)
+    })
   }
 }
 
