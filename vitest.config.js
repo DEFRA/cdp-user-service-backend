3cac1fc5a50f44b1e5af7aaf52f635c971f02681
Phil Segal <phil@phisec.co.uk>
2025-08-08 09:19:03 +0100

    CORE-267: Use lcov rather than clover as clover not working with sonarcloud

--- vitest.config.js
+++ vitest.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     globals: true,
     clearMocks: true,
     coverage: {
-      reporter: ['text', 'clover']
+      reporter: ['text', 'lcov']
     },
     setupFiles: ['.vite/mongo-memory-server.js', '.vite/setup-files.js']
   }
