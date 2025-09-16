c8caf52a2d72c7325e9314fbdb18c0e6b1a1eba2
Ben Chidgey <ben@feedmypixel.com>
2025-09-16 16:56:16 +0100

    CORE-1357: Set local mongo to be replicaSet

--- .vite/mongo-memory-server.js
+++ .vite/mongo-memory-server.js
@@ -2,11 +2,14 @@ import { setup, teardown } from 'vitest-mongodb'
 
 beforeAll(async () => {
   await setup({
+    type: 'replSet',
     binary: {
       version: 'latest',
       downloadDir: './.cache/mongodb-binaries'
     },
-    serverOptions: {},
+    serverOptions: {
+      replSet: { count: 1 }
+    },
     autoStart: false
   })
   process.env.MONGO_URI = globalThis.__MONGO_URI__
