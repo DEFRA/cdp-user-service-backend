189f7eecfc8560579dfcee94d2f34bc50e4b1b2a
Ben Chidgey <ben@feedmypixel.com>
2025-09-16 17:02:17 +0100

    CORE-1357: Lint config updates

--- vitest.config.js
+++ vitest.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
@@ -6,7 +6,9 @@ export default defineConfig({
     globals: true,
     clearMocks: true,
     coverage: {
-      reporter: ['text', 'lcov']
+      reporter: ['text', 'lcov'],
+      include: ['src/**/*.js'],
+      exclude: [...configDefaults.exclude, 'src/__fixtures__/**']
     },
     setupFiles: ['.vite/mongo-memory-server.js', '.vite/setup-files.js']
   }
