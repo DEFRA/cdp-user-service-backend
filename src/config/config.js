91c8b73386ae3717970a151abb5459b06edf3b18
Ben Chidgey <ben@feedmypixel.com>
2025-09-22 09:44:30 +0100

    Default readPreference to primary

--- src/config/config.js
+++ src/config/config.js
@@ -114,7 +114,7 @@ const config = convict({
           'secondaryPreferred',
           'nearest'
         ],
-        default: 'secondaryPreferred'
+        default: 'primary'
       }
     }
   },
