a8fd2faeb9748dcdb147e13c84faaf07b9239ae0
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2025-12-18 15:39:08 +0000

    CORE-1935 removes scope CRUD controllers

--- src/api/scopes/routes.js
+++ src/api/scopes/routes.js
@@ -1,10 +1,7 @@
 import { adminAddScopeToTeamController } from './controllers/admin/add-scope-to-team.js'
-import { adminCreateScopeController } from './controllers/admin/create-scope.js'
-import { adminDeleteScopeController } from './controllers/admin/delete-scope.js'
 import { adminGetScopeController } from './controllers/admin/get-scope.js'
 import { adminGetScopesController } from './controllers/admin/get-scopes.js'
 import { adminRemoveScopeFromTeamController } from './controllers/admin/remove-scope-from-team.js'
-import { adminUpdateScopeController } from './controllers/admin/update-scope.js'
 import { getScopesForUserController } from './controllers/get-scopes-for-user.js'
 import { adminRemoveScopeFromUserController } from './controllers/admin/remove-scope-from-user.js'
 import { adminGetScopeByNameController } from './controllers/admin/get-scope-by-name.js'
@@ -34,11 +31,6 @@ const scopes = {
           ...adminGetScopesController
         },
         {
-          method: 'POST',
-          path: '/scopes/admin',
-          ...adminCreateScopeController
-        },
-        {
           method: 'GET',
           path: '/scopes/admin/{scopeId}',
           ...adminGetScopeController
@@ -47,16 +39,6 @@ const scopes = {
           method: 'GET',
           path: '/scopes/admin/name/{scopeName}',
           ...adminGetScopeByNameController
-        },
-        {
-          method: 'PATCH',
-          path: '/scopes/admin/{scopeId}',
-          ...adminUpdateScopeController
-        },
-        {
-          method: 'DELETE',
-          path: '/scopes/admin/{scopeId}',
-          ...adminDeleteScopeController
         },
         {
           method: 'PATCH',
