6278fe16c403d5735871ec31e0c34098664b469f
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2026-01-07 14:03:48 +0000

    refactors migration endpoints

--- src/api/permissions/helpers/relationships/backfill.js
+++ src/api/permissions/helpers/relationships/backfill.js
@@ -1,15 +1,29 @@
 import Joi from 'joi'
 import { strictRelationshipSchema } from './relationship-schema.js'
+import { createLogger } from '../../../../helpers/logging/logger.js'
+
+const logger = createLogger()
 
 async function backfill(db) {
   await db.collection('relationships').drop()
   const users = await db.collection('users').find().toArray()
   const scopes = await db.collection('scopes').find().toArray()
 
+  const teamIds = new Set(
+    (await db.collection('teams').find().project({ _id: 1 }).toArray()).map(
+      (t) => t._id
+    )
+  )
+
   const bulkInsert = []
 
   for (const user of users) {
     for (const team of user.teams) {
+      if (!teamIds.has(team)) {
+        logger.warn(`${user._id} belongs to ${team} which does not exist`)
+        continue
+      }
+
       bulkInsert.push({
         subject: user._id,
         subjectType: 'user',
@@ -32,6 +46,11 @@ async function backfill(db) {
     }
 
     for (const team of scope.teams) {
+      if (!teamIds.has(team.teamId)) {
+        logger.warn(`${team.teamId} does not exist`)
+        continue
+      }
+
       bulkInsert.push({
         subject: team.teamId,
         subjectType: 'team',
--- src/api/permissions/routes.js
+++ src/api/permissions/routes.js
@@ -1,11 +1,10 @@
 import { getScopesForUserController } from './controllers/get-scopes-for-user.js'
 import { getActiveBreakGlassScopeForUser } from './controllers/get-active-break-glass-scope-for-user.js'
-import { originalScopesForUser } from './helpers/original-scopes-for-user.js'
-
-import { generateMermaidDiagram } from './helpers/relationships/mermaid-diagram.js'
-import { compareScopesOverview } from './helpers/relationships/check-scope-migration.js'
-import { backfill } from './helpers/relationships/backfill.js'
-import { scopesForUser } from './helpers/relationships/legacy-scopes-for-user.js'
+import { mermaidDiagramController } from './controllers/mermaid-diagram-controller.js'
+import {
+  backfillController,
+  checkBackfillController
+} from './controllers/migration-controllers.js'
 
 const permissions = {
   plugin: {
@@ -24,55 +23,21 @@ const permissions = {
         },
         {
           method: 'GET',
-          path: '/auth/perms',
-          ...permsController
-        },
-        {
-          method: 'GET',
-          path: '/auth/graph',
-          ...graphController
+          path: '/auth/diagram',
+          ...mermaidDiagramController
         },
         {
           method: 'GET',
           path: '/auth/backfill',
           ...backfillController
+        },
+        {
+          method: 'GET',
+          path: '/auth/check-backfill',
+          ...checkBackfillController
         }
       ])
     }
-  }
-}
-
-const backfillController = {
-  options: {},
-  handler: async (request, h) => {
-    await backfill(request.db)
-    return h.response({}).code(200)
-  }
-}
-
-const permsController = {
-  options: {},
-  handler: async (request, h) => {
-    const user = request.query.user
-
-    if (user) {
-      const v2Perms = await scopesForUser(request.db, user)
-      const v1Perms = await originalScopesForUser({ id: user }, request.db)
-      return h.response({ v2: v2Perms, v1: v1Perms }).code(200)
-    } else {
-      const allPerms = await compareScopesOverview(request.db)
-      return h.response(allPerms).code(200)
-    }
-  }
-}
-
-const graphController = {
-  options: {},
-  handler: async (request, h) => {
-    const user = request.query.user
-
-    const mermaid = await generateMermaidDiagram(request.db, user)
-    return h.response(mermaid).type('text/plain').code(200)
   }
 }
 
